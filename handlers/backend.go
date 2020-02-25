4cec9c6c8928ceec091b9a72779ec3add03f6e0f
Martin Tournoij <martin@arp242.net>
2020-02-25 12:04:01 +0800

    Add ref_stat

--- handlers/backend.go
+++ handlers/backend.go
@@ -114,6 +114,7 @@ func (h backend) Mount(r chi.Router, db zdb.DB) {
 		{
 			ap := a.With(loggedInOrPublic)
 			ap.Get("/", zhttp.Wrap(h.index))
+			ap.Get("/byrefs", zhttp.Wrap(h.byrefs))
 			ap.Get("/refs", zhttp.Wrap(h.refs))
 			ap.Get("/pages", zhttp.Wrap(h.pages))
 			ap.Get("/browsers", zhttp.Wrap(h.browsers))
@@ -346,6 +347,71 @@ func (h backend) index(w http.ResponseWriter, r *http.Request) error {
 	}{newGlobals(w, r), sr, r.URL.Query().Get("hl-period"), start, end, filter,
 		pages, refs, moreRefs, total, totalDisplay, browsers, totalBrowsers,
 		subs, sizeStat, totalSize, locStat, totalLoc, showMoreLoc})
+	l = l.Since("zhttp.Template")
+	l.FieldsSince().Print("")
+	return x
+}
+
+func (h backend) byrefs(w http.ResponseWriter, r *http.Request) error {
+	site := goatcounter.MustGetSite(r.Context())
+
+	// Cache much more aggressively for public displays. Don't care so much if
+	// it's outdated by an hour.
+	if site.Settings.Public && goatcounter.GetUser(r.Context()).ID == 0 {
+		w.Header().Set("Cache-Control", "public,max-age=3600")
+		w.Header().Set("Vary", "Cookie")
+	}
+
+	var (
+		now   = time.Now().In(site.Settings.Timezone.Loc())
+		start = now.Add(-7 * day)
+		end   = now
+	)
+	if d := r.URL.Query().Get("period-start"); d != "" {
+		var err error
+		start, err = time.Parse("2006-01-02", d)
+		if err != nil {
+			zhttp.FlashError(w, "Invalid start date: %q", d)
+			start = now.Add(-7 * day)
+		}
+	}
+	if d := r.URL.Query().Get("period-end"); d != "" {
+		var err error
+		end, err = time.Parse("2006-01-02", d)
+		if err != nil {
+			zhttp.FlashError(w, "Invalid end date: %q", d)
+			end = now
+		}
+	}
+
+	filter := r.URL.Query().Get("filter")
+	l := zlog.Module("backend").Field("site", site.ID)
+
+	// XXX
+	var refs goatcounter.Stats
+	total, totalDisplay, err := refs.ListRefs(r.Context(), start, end)
+	if err != nil {
+		return err
+	}
+	l = l.Since("refs.List")
+
+	subs, err := site.ListSubs(r.Context())
+	if err != nil {
+		return err
+	}
+
+	x := zhttp.Template(w, "backend_refs.gohtml", struct {
+		Globals
+		SelectedPeriod   string
+		PeriodStart      time.Time
+		PeriodEnd        time.Time
+		Filter           string
+		Refs             goatcounter.Stats
+		TotalHits        int
+		TotalHitsDisplay int
+		SubSites         []string
+	}{newGlobals(w, r), r.URL.Query().Get("hl-period"), start, end, filter,
+		refs, total, totalDisplay, subs})
 	l = l.Since("zhttp.Template")
 	l.FieldsSince().Print("")
 	return x
